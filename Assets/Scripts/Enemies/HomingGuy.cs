82e28bd2b3afde00da1004cb2bae6ec547fa7469
SmkMth <dandavidnoon@gmail.com>
2018-10-22 19:37:00 +0100

    no
    no

---
+++ Assets/Scripts/Enemies/HomingGuy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingGuy : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 movementVector;
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+        movementVector = player.transform.position - gameObject.transform.position;
+        rb.AddForce(movementVector);
+	}
+}
